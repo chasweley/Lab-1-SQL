a043c52a8e59a86f7a05bbf73625bb3ea89847e9
Charlotte <charlotteleyser@icloud.com>
2023-12-10 23:08:33 +0100

    -Added comments -Corrected errors when trying to add to database

--- AddInfoToDatabase.cs
+++ AddInfoToDatabase.cs
@@ -10,52 +10,55 @@ namespace Lab_1_SQL
 {
     internal class AddInfoToDatabase
     {
+        //Method to add new personnel to database
         static internal void AddNewPersonnel(SqlConnection connection)
         {
             Console.Clear();
-            //Fixa ev. felhantering och optional på category
-            string sqlQuery = "INSERT INTO Personnel VALUES (@FirstName, @LastName, @Category)";
+            string sqlQuery = "INSERT INTO Personnel (FirstName, LastName) VALUES (@FirstName, @LastName)";
+
             using (SqlCommand addNewPersonnelCommand = new SqlCommand(sqlQuery, connection))
             {
                 Console.Write("First name: ");
                 string firstName = Console.ReadLine();
                 Console.Write("Last name: ");
                 string lastName = Console.ReadLine();
-                Console.Write("Category: ");
-                string category = Console.ReadLine();
 
+                //Gives variables correct placement in table
                 addNewPersonnelCommand.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar, 50).Value = firstName;
-                addNewPersonnelCommand.Parameters.Add(@"LastName", System.Data.SqlDbType.NVarChar, 50).Value = lastName;
-                addNewPersonnelCommand.Parameters.Add(@"Category", System.Data.SqlDbType.NVarChar, 20).Value = category;
-                addNewPersonnelCommand.ExecuteNonQuery();
+                addNewPersonnelCommand.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar, 50).Value = lastName;
+                addNewPersonnelCommand.ExecuteNonQuery(); //Actually adds to database
                 Console.Clear();
-                Console.WriteLine($"{firstName} {lastName} with category {category} added to database.");
+                Console.WriteLine($"{firstName} {lastName} was added to database.");
             }
-            Menu.ReturnToMenu();
+            Menus.ReturnToMenu();
         }
 
+        //Method to add new student to database
         static internal void AddNewStudent(SqlConnection connection)
         {
             Console.Clear();
-            //Fixa ev. felhantering och hur hantera lägga till klass?
-            string sqlQuery = "INSERT INTO Students VALUES (@FirstName, @LastName, @BirthDate)";
+
+            string sqlQuery = "INSERT INTO Students (FirstName, LastName, BirthDate) VALUES (@FirstName, @LastName, @BirthDate)";
             using (SqlCommand addNewStudentCommand = new SqlCommand(sqlQuery, connection))
             {
                 Console.Write("First name: ");
                 string firstName = Console.ReadLine();
                 Console.Write("Last name: ");
                 string lastName = Console.ReadLine();
-                Console.Write("BirthDate: ");
-                string birthDate = Console.ReadLine();
+                Console.Write("Birth date (format YYYY-MM-DD): ");
+                DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+                //Convert DateTime to DateOnly for better looking output when confirming add to database
+                DateOnly birthDateOnly = DateOnly.FromDateTime(birthDate);
 
                 addNewStudentCommand.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar, 50).Value = firstName;
-                addNewStudentCommand.Parameters.Add(@"LastName", System.Data.SqlDbType.NVarChar, 50).Value = lastName;
-                addNewStudentCommand.Parameters.Add(@"BirthDate", System.Data.SqlDbType.Date).Value = birthDate;
+                addNewStudentCommand.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar, 50).Value = lastName;
+                addNewStudentCommand.Parameters.Add("@BirthDate", System.Data.SqlDbType.Date).Value = birthDate;
                 addNewStudentCommand.ExecuteNonQuery();
                 Console.Clear();
-                Console.WriteLine($"Student {firstName} {lastName} with birthdate {birthDate} added to database.");
+                Console.WriteLine($"Student {firstName} {lastName} with birth date {birthDateOnly} was added to database.");
             }
-            Menu.ReturnToMenu();
+            Menus.ReturnToMenu();
         }
     }
 }
